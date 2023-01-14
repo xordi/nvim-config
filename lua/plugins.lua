180e92e7a89599c43fa12f1def2580bc82a1dbf5
xordi <jordi.obuades@gmail.com>
2023-01-14 19:06:56 +0100

    add navigation integrated with tmux

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -60,7 +60,7 @@ return require('packer').startup(function(use)
     end
   }
   use {"akinsho/toggleterm.nvim", tag = '*'}
-
+  use 'christoomey/vim-tmux-navigator'
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
