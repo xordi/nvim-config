467f89c72270ff2a0e3f36e3c3b44da08dad190d
xordi <jordi.buades@helloprima.com>
2023-07-28 09:29:32 +0200

    centering screen on search

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -23,6 +23,8 @@ vim.keymap.set('n', '{', '{zz', { noremap = true })
 vim.keymap.set('n', '}', '}zz', { noremap = true })
 vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true })
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true })
+vim.keymap.set('n', 'n', 'nzz', { noremap = true })
+vim.keymap.set('n', 'N', 'Nzz', { noremap = true })
 
 -- completion keymaps
 local ls = require('luasnip')
