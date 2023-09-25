99f6ca39e9f4fa3fc661bc020fd15f4a7c8535fd
xordi <jordi.buades@helloprima.com>
2023-09-25 16:24:19 +0200

    Fix luasnip usage on tab

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -21,10 +21,11 @@ return {
         ["<C-n>"] = cmp.mapping.select_next_item(),
         -- Add tab support
         ["<Tab>"] = cmp.mapping(function(fallback)
+          local luasnip = require("luasnip")
           if cmp.visible() then
             cmp.select_next_item()
-          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
-          -- they way you will only jump inside the snippet region
+            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+            -- they way you will only jump inside the snippet region
           elseif luasnip.expand_or_locally_jumpable() then
             luasnip.expand_or_jump()
           else
@@ -33,6 +34,7 @@ return {
         end, { "i", "s" }),
 
         ["<S-Tab>"] = cmp.mapping(function(fallback)
+          local luasnip = require("luasnip")
           if cmp.visible() then
             cmp.select_prev_item()
           elseif luasnip.jumpable(-1) then
