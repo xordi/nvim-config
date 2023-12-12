7a383974281d0c8cebd411a4415afc87c4e7d7aa
xordi <jordi.obuades@gmail.com>
2023-12-12 23:58:08 +0100

    Enable treesitter incremental selection

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,13 +2,22 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   "nvim-treesitter/nvim-treesitter",
   config = function()
-    require("nvim-treesitter.configs").setup {
+    require("nvim-treesitter.configs").setup({
       ensure_installed = { "rust", "vim", "lua", "elixir", "heex", "eex", "yaml", "json" },
       auto_install = true,
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
       },
-    }
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<cr>",
+          node_incremental = "grn",
+          scope_incremental = "grc",
+          node_decremental = "grm",
+        }
+      }
+    })
   end
 }
